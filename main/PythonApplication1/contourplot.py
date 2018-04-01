c7a0e6a77ce6833b12091da4b7a143800d6b798e
serhatbilyaz <serhatbilyaz@utexas.edu>
2018-04-01 03:10:12 -0500

    Started writing the plotting scripts

---
+++ main/PythonApplication1/contourplot.py
@@ -0,0 +1,21 @@
+# This script plots the temperature contours
+
+import numpy as np
+import matplotlib
+from matplotlib import pyplot
+
+# Plotting
+# Plotting Temp    
+pyplot.figure()
+
+#Instead of these ones, fancy colorful contour graphs will be shown
+
+#pyplot.plot(X,Uy,'--',linewidth=3, label="OpenFoam")
+#pyplot.plot(ExpX_Uy,ExpUy,linewidth=3, label="Exp")
+
+#bounds=np.array([0, 0.08])
+#pyplot.xlim(bounds) 
+#pyplot.xlabel('Position (m)', fontsize=10)
+#pyplot.ylabel('$U_y$ (m/s)', fontsize=10)
+#pyplot.legend(loc='upper left')
+#pyplot.savefig('Uy.pdf', bbox_inches='tight')
